041b4aca81f4bf803850cbb42643a59fc76dfc9c
lkdhruw <lkdhruw@live.com>
2020-06-21 00:54:07 +0530

    Unfinished commit 20-06-20

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PIL import Image
 from PIL import ImageFilter
+from PIL import ImageTk
 import io
 import re
 import cv2
@@ -16,26 +17,23 @@ class Window:
         self.filename = ""
         self.commandText = ""
 
-        #toolbar = Frame(root)
-        #toolbar.grid(row=1, column=1)
+        # toolbar = Frame(root)
+        # toolbar.grid(row=1, column=1)
 
         self.browseButton = Button(root, text="Browse", command=self.browseimg)
         self.browseButton.grid(row=1, column=1)
 
-        #im = Image.open("lena.png")
-        im = Image.open("Default.png")
+        # im = Image.open("Default.png")
+        im = cv2.imread('Default.png')
         self.originalImage = im
-
-        #self.originalImage480x480 = self.originalImage
-        #self.originalImage480x480.thumbnail((480, 480))
-        self.originalImage360x360 = self.originalImage
-        self.originalImage360x360.thumbnail((360, 360))
-        #self.originalImage240x240 = self.originalImage
-        #self.originalImage240x240.thumbnail((240, 240))
+        # https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html#resize
+        # self.originalImage480x480 = cv2.resize(im, (480, 480), interpolation=cv2.INTER_AREA)
+        self.originalImage360x360 = cv2.resize(im, (360, 360), interpolation=cv2.INTER_AREA)
         self.cvImage = None
 
         image = self.originalImage360x360
-        photo = self.imageToBytes(image)
+        # photo = self.imageToBytes(image)
+        photo = self.arrayToImage(image)  # openCV to PIL image
         self.originalImageView = Label(root, width=400, height=360)
         self.originalImageView.grid(row=2, column=1)
         self.originalImageView["image"] = photo
@@ -57,119 +55,103 @@ class Window:
     def is_grayscale(self, image):
         return len(image.shape) < 3
 
-    def grayscale(self,image):
-        #image = self.originalImage360x360
-        image = image.convert("L")
-        photo = self.imageToBytes(image)
+    def grayscale(self, image):
+        if not self.is_grayscale(image):
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        photo = self.arrayToImage(image)
         self.modifiedImageView["image"] = photo
         self.modifiedPhoto = photo
         return image
 
     def quantize(self, image, quantizeValue):
-        #image = self.originalImage360x360
+        # bug: the whole programme is now using
+        # an opencv image not PIL image object
+        # Need to resolve
         image = image.quantize(quantizeValue)
         photo = self.imageToBytes(image)
         self.modifiedImageView["image"] = photo
         self.modifiedPhoto = photo
         return image
 
-    def binary(self, image, threshold):
-        #image = self.originalImage360x360
-        image = image.convert("L")
-        width, _ = image.size
-        black = 1
-        white = 1
-        for i, px in enumerate(image.getdata()):
-            y = int(i / width)
-            x = int(i % width)
-            if px > threshold:
-                image.putpixel((x, y), 255)
-                white += 1
-            else:
-                image.putpixel((x, y), 0)
-                black += 1
-        photo = self.imageToBytes(image)
+    def binary(self, image, threshold, thresh_type=cv2.THRESH_BINARY):
+        # https://docs.opencv.org/4.2.0/d7/d4d/tutorial_py_thresholding.html
+        if not self.is_grayscale(image):
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        _, threshold_image = cv2.threshold(image, threshold, 255, thresh_type)
+        photo = self.arrayToImage(threshold_image)
         self.modifiedImageView["image"] = photo
         self.modifiedPhoto = photo
-        return image
+        return threshold_image
 
-    def extract_feature(self, originalImage, threshold):
-        image = originalImage.copy()
-        binImage = self.binary(image,threshold)
-        if image.mode == "RGB":
-            width, _ = image.size
-            for i, px in enumerate(binImage.getdata()):
-                y = int(i / width)
-                x = int(i % width)
-                if px == 0:
-                    image.putpixel((x, y), (px, 0, 0))
+    def adaptive_threshold(self, image, thresh_type=cv2.THRESH_BINARY, **kwargs):
+        # https://docs.opencv.org/4.2.0/d7/d4d/tutorial_py_thresholding.html
+        if not self.is_grayscale(image):
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-            photo = self.imageToBytes(image)
+        adaptive_method = cv2.ADAPTIVE_THRESH_GAUSSIAN_C
+        block_size = 11
+        constant = 2
+        for key, value in kwargs.items():
+            if key == 'adaptive_method':
+                # cv.ADAPTIVE_THRESH_MEAN_C
+                # cv.ADAPTIVE_THRESH_GAUSSIAN_C
+                if value == 'MEAN':
+                    adaptive_method = cv2.ADAPTIVE_THRESH_MEAN_C
+                elif value == 'GAUSSIAN':
+                    adaptive_method = cv2.ADAPTIVE_THRESH_GAUSSIAN_C
+            elif key == 'block_size':
+                block_size = int(float(value))
+            elif key == 'constant':
+                constant = int(float(value))
 
-            self.modifiedImageView["image"] = photo
-            self.modifiedPhoto = photo
-
-        return image
-
-    def rgb(self, originalImage, options):
-        image = originalImage.copy()
-        if image.mode == "RGB":
-            R, G, B = image.split()
-            width, _ = image.size
-            listR = list(R.getdata())
-            listG = list(G.getdata())
-            listB = list(B.getdata())
-
-            if options == "r":
-                for i, px in enumerate(R.getdata()):
-                    y = int(i / width)
-                    x = int(i % width)
-                    image.putpixel((x, y), (px, 0, 0))
-                photo = self.imageToBytes(image)
-            elif options == "g":
-                for i, px in enumerate(G.getdata()):
-                    y = int(i / width)
-                    x = int(i % width)
-                    image.putpixel((x, y), (0, px, 0))
-                photo = self.imageToBytes(image)
-            elif options == "b":
-                for i, px in enumerate(B.getdata()):
-                    y = int(i / width)
-                    x = int(i % width)
-                    image.putpixel((x, y), (0, 0, px))
-                photo = self.imageToBytes(image)
-            elif options == "gb":
-                for i, px in enumerate(G.getdata()):
-                    y = int(i / width)
-                    x = int(i % width)
-                    image.putpixel((x, y), (0, px, listB[i]))
-                photo = self.imageToBytes(image)
-            elif options == "rb":
-                for i, px in enumerate(B.getdata()):
-                    y = int(i / width)
-                    x = int(i % width)
-                    image.putpixel((x, y), (listR[i], 0, px))
-                photo = self.imageToBytes(image)
-            elif options == "rg":
-                for i, px in enumerate(R.getdata()):
-                    y = int(i / width)
-                    x = int(i % width)
-                    image.putpixel((x, y), (px, listG[i], 0))
-                photo = self.imageToBytes(image)
-            else:
-                photo = self.imageToBytes(image)
-
-            self.modifiedImageView["image"] = photo
-            self.modifiedPhoto = photo
-
-        return image
-
-    def blur(self, image):
-        image = image.filter(ImageFilter.BLUR)
-        photo = self.imageToBytes(image)
+        threshold_image = cv2.adaptiveThreshold(image, 255,
+                                                adaptive_method, thresh_type,
+                                                block_size, constant)
+        photo = self.arrayToImage(threshold_image)
         self.modifiedImageView["image"] = photo
         self.modifiedPhoto = photo
-        return image
+        return threshold_image
+
+    def blur(self, image, ksize):
+        # https://docs.opencv.org/4.2.0/d4/d13/tutorial_py_filtering.html
+        # Need implement more filters such as gaussian, median etc
+        blur_image = cv2.blur(image, (ksize, ksize))
+        photo = self.arrayToImage(blur_image)
+        self.modifiedImageView["image"] = photo
+        self.modifiedPhoto = photo
+        return blur_image
+
+    def blobs(self, image, **kwargs):
+        # https://docs.opencv.org/4.2.0/d0/d7a/classcv_1_1SimpleBlobDetector.html
+        # https://www.geeksforgeeks.org/find-circles-and-ellipses-in-an-image-using-opencv-python/
+        # https://www.learnopencv.com/blob-detection-using-opencv-python-c/
+        if not self.is_grayscale(image):
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        params = cv2.SimpleBlobDetector_Params()
+
+        params.filterByArea = True
+        params.minArea = 100
+
+        params.filterByCircularity = True
+        params.minCircularity = 0.9
+
+        params.filterByConvexity = True
+        params.minConvexity = 0.2
+
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.01
+        detector = cv2.SimpleBlobDetector_create(params)
+
+        keypoints = detector.detect(image)
+        blank = np.zeros((1, 1))
+        blobs = cv2.drawKeypoints(image, keypoints, blank, (0, 255, 0),
+                                  cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        photo = self.arrayToImage(blobs)
+        self.modifiedImageView["image"] = photo
+        self.modifiedPhoto = photo
+        return blobs
 
     def contour(self, image):
         image = image.filter(ImageFilter.CONTOUR)
@@ -234,7 +216,15 @@ class Window:
         self.modifiedPhoto = photo
         return image
 
-    def imageToBytes(self,image):
+    def arrayToImage(self, image):
+        if not self.is_grayscale(image):
+            b, g, r = cv2.split(image)
+            image = cv2.merge((r, g, b))
+        p = Image.fromarray(image)
+        photo = ImageTk.PhotoImage(image=p)
+        return photo
+
+    def imageToBytes(self, image):
         b = io.BytesIO()
         image.save(b, 'gif')
         p = b.getvalue()
@@ -247,26 +237,22 @@ class Window:
         self.filename = askopenfilename()
 
         if len(self.filename) > 0:
-            self.originalImage = Image.open(self.filename)
-            pil_image = self.originalImage.copy()
-            open_cv_image = np.array(pil_image.convert('RGB'))
-            self.cvImage = open_cv_image[:, :, ::-1].copy() # cv2.imread(self.filename, 0)
-            #self.originalImage480x480 = self.originalImage
-            #self.originalImage480x480.thumbnail((480, 480))
-            self.originalImage360x360 = self.originalImage
-            self.originalImage360x360.thumbnail((360, 360))
-            #self.originalImage240x240 = self.originalImage
-            #self.originalImage240x240.thumbnail((240, 240))
+            img = cv2.imread(self.filename)
+            self.originalImage = img
+
+            # self.originalImage480x480 = cv2.resize(im, (480, 480), interpolation=cv2.INTER_AREA)
+            self.originalImage360x360 = cv2.resize(img, (360, 360), interpolation=cv2.INTER_AREA)
 
             image = self.originalImage360x360
-            photo = self.imageToBytes(image)
+            photo = self.arrayToImage(image)
             self.originalImageView["image"] = photo
             self.originalPhoto = photo
             self.modifiedImageView["image"] = photo
             self.modifiedPhoto = photo
+            self.applycommand()
 
     def applycommand(self):
-        self.commandText = self.command.get(1.0,END)
+        self.commandText = self.command.get(1.0, END)
         inputcommand = self.commandText
         # print inputCommand
         commands = inputcommand.split("\n")
@@ -280,18 +266,39 @@ class Window:
                     mod_image = self.grayscale(mod_image)
                 elif re.search(r'quantize\((\d+)\)', command, re.I):
                     m = re.search(r'quantize\((\d+)\)', command, re.I)
-                    mod_image = self.quantize(mod_image,int(float(m.group(1))))
+                    mod_image = self.quantize(mod_image, int(float(m.group(1))))
                 elif re.search(r'binary\((\d+)\)', command, re.I):
                     m = re.search(r'binary\((\d+)\)', command, re.I)
-                    mod_image = self.binary(mod_image,int(float(m.group(1))))
+                    mod_image = self.binary(mod_image, int(float(m.group(1))))
+                elif re.search(r'adaptive_threshold\((.*)\)', command, re.I):
+                    m = re.search(r'adaptive_threshold\((.*)\)', command, re.I)
+                    options = ((m.group(1)).replace(' ', '')).split(',')
+                    kwargs = {}
+                    for option in options:
+                        key, value = option.split('=')
+                        kwargs[key] = value
+                    mod_image = self.adaptive_threshold(mod_image, **kwargs)
                 elif re.search(r'rgb\((\w+)\)', command, re.I):
                     m = re.search(r'rgb\((\w+)\)', command, re.I)
-                    mod_image = self.rgb(mod_image,m.group(1))
+                    mod_image = self.rgb(mod_image, m.group(1))
                 elif re.search(r'extract_feature\((\d+)\)', command, re.I):
                     m = re.search(r'extract_feature\((\d+)\)', command, re.I)
-                    mod_image = self.extract_feature(mod_image,int(float(m.group(1))))
-                elif command == "blur":
-                    mod_image = self.blur(mod_image)
+                    mod_image = self.extract_feature(mod_image, int(float(m.group(1))))
+                elif re.search(r'blur\((\d+)\)', command, re.I):
+                    m = re.search(r'blur\((\d+)\)', command, re.I)
+                    mod_image = self.blur(mod_image, int(float(m.group(1))))
+                elif re.search(r'blobs\((.*)\)', command, re.I):
+                    m = re.search(r'blobs\((.*)\)', command, re.I)
+                    command = (m.group(1)).replace(' ', '')
+                    options = []
+                    if not command == '':
+                        options = command.split(',')
+                    kwargs = {}
+                    for option in options:
+                        key, value = option.split('=')
+                        kwargs[key] = value
+
+                    mod_image = self.blobs(mod_image, **kwargs)
                 elif command == "contour":
                     mod_image = self.contour(mod_image)
                 elif command == "detail":
@@ -312,10 +319,9 @@ class Window:
                     mod_image = self.sharpen(mod_image)
 
 
-
 root = Tk()
 
 window = Window(root)
 
 root.mainloop()
-#root.destroy()
+# root.destroy()
